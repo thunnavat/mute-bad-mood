ff9a74690c7612e07891f5f7ead52b82d56f82d4
Ftittawat <tittawatjaiou@gmail.com>
2024-01-14 21:05:24 +0700

    Revert "Revert "Create Map, San Jao, Siam Sie, Siam Sie Detail, God and God Detail""
    This reverts commit dd8f5b4559d2c6cb7b69148c15c86fb831abea5d.

---
+++ src/app/map/[sanjao]/page.tsx
@@ -0,0 +1,61 @@
+import ButtonBack from "@/app/components/ButtonBack";
+import ButtonLink from "@/app/components/ButtonLink";
+import Image from "next/image";
+import { promises as fs } from 'fs';
+
+const Sanjao = async ({ params }: { params: { sanjao: string } }) => {
+    const file = await fs.readFile(process.cwd() + '/src/app/data.json', 'utf8');
+    const data = JSON.parse(file);
+    const Location: string[] = data.location
+    const LocationDetail: string[] = data.locationdetail
+    let location: string = ""
+    let location_detail: string = ""
+    let img: string = ""
+    let url: string = ""
+    if (params.sanjao === "location1") {
+        location = Location[0]
+        location_detail = LocationDetail[0]
+        img = "/san-jao-1.png"
+        url = "/map/location1/siamsie";
+    } else if (params.sanjao === "location2") {
+        location = Location[1]
+        location_detail = LocationDetail[1]
+        img = "/san-jao-2.png"
+        url = "/map/location2/siamsie";
+    } else if (params.sanjao === "location3") {
+        location = Location[2]
+        location_detail = LocationDetail[2]
+        img = "/san-jao-3.png"
+        url = "/map/location3/siamsie";
+    }
+    return (
+        <main className="h-screen w-screen flex flex-col">
+            <ButtonBack 
+            href="/map"/>
+            <div className="flex flex-col h-screen w-screen items-center">
+                <div className="m-5">
+                    <Image
+                    src={img}
+                    alt="san-jao"
+                    height={160}
+                    width={160}
+                    />
+                </div>
+                <div className="flex justify-center items-center max-w-md w-10/12 bg-white rounded-[50px] border-[7px] border-solid border-black shadow-[5px_3px_0px_#000000]">
+                    <h1 className="text-2xl font-semibold capitalize p-2">{location}</h1>
+                </div>
+                <div className="flex flex-col justify-center items-center mt-10 w-10/12 max-w-md bg-white rounded-[50px] border-[7px] border-solid border-black shadow-[5px_3px_0px_#000000]">
+                    <h2 className="text-2xl font-semibold pt-5">ประวัติ</h2>
+                    <p className="text-justify mx-5 mt-5 mb-10 tracking-wide">{location_detail}</p>
+                </div>
+            </div>
+            <div className="mb-10">
+                <ButtonLink
+                    text="next"
+                    href={url}/>
+            </div>
+        </main>
+    )
+}
+
+export default Sanjao;
