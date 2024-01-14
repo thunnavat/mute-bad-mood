f4422c3c6252d3a9de0c7fe926cf64fb92b40879
Ftittawat <tittawatjaiou@gmail.com>
2024-01-14 21:02:33 +0700

    Create Map, San Jao, Siam Sie, Siam Sie Detail, God and God Detail

--- src/app/components/ButtonLink.tsx
+++ src/app/components/ButtonLink.tsx
@@ -1,19 +1,21 @@
+'use client'
 import Link from "next/link"
 import React from "react"
 
 interface IButtonLinkProps {
   text: string
   href: string
+  disabled?: boolean
 }
 
-const ButtonLink = ({ text, href }: IButtonLinkProps) => {
+const ButtonLink = ({ text, href, disabled }: IButtonLinkProps) => {
   return (
     <Link
       href={href}
-      className="mx-auto flex items-center justify-center"
+      className="flex items-center justify-center"
     >
-      <button className="absolute bottom-10 w-full max-w-60 rounded-[48px] bg-gray-800">
-        <p className="p-3 uppercase text-white">{text}</p>
+      <button className="my-5 w-10/12 max-w-md rounded-[48px] bg-gray-800 disabled:opacity-0" disabled={disabled}>
+        <p className="p-4 uppercase text-white font-bold text-xl">{text}</p>
       </button>
     </Link>
   )
