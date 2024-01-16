18ae74ddda39229f57f4d07a4d220f33caa48742
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2024-01-17 01:16:59 +0700

    feat: light image in /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,60 @@
+"use client"
 import Image from "next/image"
 import ButtonLink from "./components/ButtonLink"
 
 const Home = () => {
+  if (typeof window !== "undefined") {
+    window.addEventListener("load", () => {
+      const frames = document.getElementById("animation")
+        ?.children as HTMLCollectionOf<HTMLElement>
+      const frameCount = frames?.length
+      let f = 0
+      setInterval(() => {
+        if (frames && frameCount) {
+          frames[f % frameCount].style.display = "none"
+          frames[++f % frameCount].style.display = "block"
+        }
+      }, 100)
+    })
+  }
+
+  const renderLightImage = () => {
+    const lightImages = []
+    for (let i = 0; i <= 15; i++) {
+      lightImages.push(
+        <Image
+          key={i}
+          src={`/light/light${i}.svg`}
+          alt="light"
+          width="0"
+          height="0"
+          sizes="100vw"
+          priority
+          className="h-full w-[500px] max-w-[1536px]"
+        />
+      )
+    }
+    return lightImages
+  }
+
   return (
     <main className="h-full w-full">
       <div className="flex min-h-screen items-center justify-center">
+        <div
+          id="animation"
+          className="fixed top-6 hidden"
+        >
+          {renderLightImage()}
+        </div>
         <Image
+          id="logo"
           src="/Logo.png"
           alt="Logo"
           width="0"
           height="0"
           sizes="100vw"
           priority
-          className="fixed z-10 h-auto w-[200px]"
+          className="fixed z-10 mb-32 h-auto w-[100px]"
         />
         <Image
           src="/Cloud.png"
@@ -21,7 +63,7 @@ const Home = () => {
           height="0"
           sizes="100vw"
           priority
-          className="fixed left-0 top-32 h-auto w-[150px]"
+          className="cloud fixed left-0 top-32 hidden h-auto w-[120px] ip:w-[180px]"
         />
         <Image
           src="/Cloud.png"
@@ -30,7 +72,7 @@ const Home = () => {
           height="0"
           sizes="100vw"
           priority
-          className="fixed bottom-32 left-8 h-auto w-[150px]"
+          className="cloud fixed bottom-32 left-8 hidden h-auto w-[120px] ip:w-[180px]"
         />
         <Image
           src="/Cloud.png"
@@ -39,7 +81,7 @@ const Home = () => {
           height="0"
           sizes="100vw"
           priority
-          className="fixed -right-14 bottom-80 scale-x-[-1] h-auto w-[150px]"
+          className="cloud fixed -right-[70px] bottom-80 hidden h-auto w-[120px] scale-x-[-1] ip:w-[180px]"
         />
         <ButtonLink
           text="start"
