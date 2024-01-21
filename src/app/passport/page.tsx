1ca8b5a34dd65889dbcac1d6b7c5eca47e328bae
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2024-01-22 01:09:43 +0700

    fix: page style

--- src/app/passport/page.tsx
+++ src/app/passport/page.tsx
@@ -22,9 +22,9 @@ const PassportPage = () => {
   return (
     <>
       <SlideTransition>
-        <main className="h-full w-full overflow-x-hidden bg-custom-200">
-          <div className="relative h-screen sm:grid sm:grid-cols-1 sm:items-center">
-            <div className="absolute -right-28 -top-32 z-10 max-se:-right-36 ip:top-4 sm:relative sm:flex sm:justify-center sm:left-0 sm:top-0">
+        <main className="bg-custom-200 h-full w-full overflow-x-hidden">
+          <div className="relative h-screen sm:grid sm:grid-cols-1 sm:content-center">
+            <div className="absolute -right-28 -top-32 z-10 max-se:-right-36 ip:top-4 sm:relative sm:left-0 sm:top-0 sm:flex sm:justify-center">
               <div className="relative">
                 <Image
                   src="/Passsport-open.svg"
@@ -57,7 +57,7 @@ const PassportPage = () => {
                   />
                 </div>
               </div>
-            </div>
+                          </div>
             <div className="relative h-full sm:h-auto">
               <Image
                 src="/Passsport-close.svg"
@@ -68,7 +68,7 @@ const PassportPage = () => {
                 priority
                 className="absolute -left-24 top-12 h-auto w-[350px] max-se:top-28 ip:top-44 sm:hidden"
               />
-              <ButtonLink
+<ButtonLink
                 href="/map"
                 text="next"
                 className={`${
