bc71ab37e68a16f72091a2f18f314dd437bb78b6
Ftittawat <tittawatjaiou@gmail.com>
2024-01-21 23:09:29 +0700

    fix: share

--- src/app/sharecard/[id]/page.tsx
+++ src/app/sharecard/[id]/page.tsx
@@ -1,22 +1,27 @@
-"use client"
 import ButtonLink from "@/app/components/ButtonLink"
-import ButtonDownload from "@/app/components/ButtonDownload"
 import Image from "next/image"
-import {
-  FacebookShareButton,
-  FacebookIcon,
-  TelegramShareButton,
-  TelegramIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  LineShareButton,
-  LineIcon
-} from "next-share"
+import type { Metadata } from "next"
 
-const SiamSieDetail = ({
+export const metadata: Metadata = {
+  metadataBase: new URL('https://mute-bad-mood.vercel.app/'),
+  openGraph: {
+    title: "MUTE MOOD",
+    description: "Website for making wishes",
+    images: [
+      {
+        url: "card/sharecard/1.png",
+        width: 1200,
+        height: 630,
+        alt: "MUTE MOOD"
+      }
+    ]
+  }
+}
+
+const ShareCard = ({
   params
 }: {
-  params: { sanjao: string; siamsiedetail: number }
+  params: { id: number }
 }) => {
   const carddetail: string[] = [
     "ดวงของคุณช่วงนี้กำลังจะมา เป็นโอกาสที่ดีในการริเริ่มการงานใหม่ มีเงินไว้ใช้ไว้กินสบายๆ",
@@ -32,14 +37,13 @@ const SiamSieDetail = ({
     "คนโสด จะยังเป็นโสดอยู่ ควรรักตัวเองให้มากๆก่อนจะเปิดใจรับใครเข้ามา เป็นตัวของตัวเองไม่ต้องฝืนปรับเปลี่ยน อะไรเพื่อคนอื่นแล้วคู่ในอนาคตจะเข้ามาหาเอง",
     "คนโสดคุณมีคนแอบปลื้มแต่เขาไม่กล้า เข้าหาลองใส่ใจคนรอบตัวให้มากกว่านี้ คนนั้นอาจจะเป็นคนที่คุณสนิทมากกว่าที่คิด"
   ]
-  const iconsize: number = 48
-  const baseURL: string = "https://mute-bad-mood.vercel.app/"
+
   return (
     <main className="max-w-screen flex h-auto min-h-screen flex-col items-center justify-center bg-[#99c2eb]">
       <div className="mx-5 mt-5 flex h-auto w-[300px] flex-col items-center justify-center rounded-3xl bg-white p-2 shadow-2xl">
         <div>
           <Image
-            src={`/card/${params.siamsiedetail}.png`}
+            src={`/card/${params.id}.png`}
             alt="card"
             width={300}
             height={0}
@@ -49,66 +53,17 @@ const SiamSieDetail = ({
         </div>
         <div className="mx-5 mb-2 flex text-center ">
           <p className="font-anuphan text-xs font-medium">
-            {carddetail[params.siamsiedetail - 1]}
+            {carddetail[params.id - 1]}
           </p>
         </div>
       </div>
-      <div className="mt-5 flex flex-row space-x-2">
-        <FacebookShareButton
-          url={`${baseURL}/sharecard/${params.siamsiedetail}`}
-          quote={"next-share is a social share buttons plugin for React apps."}
-          hashtag={"#mutemood"}
-        >
-          <FacebookIcon
-            size={iconsize}
-            round
-          />
-        </FacebookShareButton>
-        <LineShareButton
-          url={"https://mute-bad-mood.vercel.app/"}
-          title={
-            "next-share is a social share buttons for your next React apps."
-          }
-        >
-          <LineIcon
-            size={iconsize}
-            round
-          />
-        </LineShareButton>
-        <TelegramShareButton
-          url={"https://mute-bad-mood.vercel.app/"}
-          title={
-            "next-share is a social share buttons for your next React apps."
-          }
-        >
-          <TelegramIcon
-            size={iconsize}
-            round
-          />
-        </TelegramShareButton>
-        <TwitterShareButton
-          url={"https://mute-bad-mood.vercel.app/"}
-          title={
-            "next-share is a social share buttons for your next React apps."
-          }
-        >
-          <TwitterIcon
-            size={iconsize}
-            round
-          />
-        </TwitterShareButton>
-      </div>
-      <ButtonDownload
-        cardNumber={params.siamsiedetail}
-        className="mt-5"
-      />
       <ButtonLink
-        text="play again"
-        href="/map"
-        className="bottom-0"
+        text="play"
+        href="/"
+        className="mt-16 bottom-0"
       />
     </main>
   )
 }
 
-export default SiamSieDetail
+export default ShareCard
